dc3c04da546d1c197b5b77232c775866d0181590
Mary-Kate James <marykatj@gmail.com>
2017-12-30 21:19:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -14,7 +14,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 
 function addElementToEndOfArray(array, element) {
   const myArray = array;
-    const myNewArray = new Array(myArray...`${element}`);
+    const myNewArray = new Array(...myArray, `${element}`);
   return myNewArray;
 }
 
