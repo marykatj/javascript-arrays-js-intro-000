68716209a3610dcf324f4c4de43a0305721adfa4
Mary-Kate James <marykatj@gmail.com>
2017-12-30 20:44:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,8 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-  var myArray = `${array}`
-    var myNewArray = new Array([`${element}`, ...myArray]);
+  const myArray = `${array}`
+    const myNewArray = new Array([`${element}`, ...myArray]);
   return new Array();
 }
 
